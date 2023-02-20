ba5f12f1abed99d4c398cfe9a7aa52fe91cf3b68
revalew <115182776+revalew@users.noreply.github.com>
2023-02-20 22:59:11 +0100

    finishing touches with JavaScript - finished website

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const nav = document.querySelector('.nav')
+const navBar = document.querySelector('.navBar')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 allNavItems.forEach(item => {
 	// this was originally in the handleNav function BUT it would execute the forEach loop and add the listener every time the burger button is clicked so it was not optimal in my opinion and I disagree with the "original" implementation presented during the course
@@ -11,6 +15,7 @@ allNavItems.forEach(item => {
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
+	navBtnBars.classList.remove('black-bars-color')
 	handleNavAnimation()
 	// deleteNavAnimation()
 }
@@ -40,4 +45,24 @@ const handleNavAnimation = () => {
 // 	})
 // }
 
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+const handleObserver = () => {
+	const currentSection = window.scrollY
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 45) {
+			navBtnBars.classList.add('black-bars-color')
+			// navBar.classList.add('bg-shadow')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 5) {
+			navBtnBars.classList.remove('black-bars-color')
+			// navBar.classList.remove('bg-shadow')
+		}
+	})
+}
+
+handleCurrentYear()
 navBtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
